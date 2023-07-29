927facefc451e065dd0c06bdf434a44cf0f77c87
freakzlike <freakzlike@gmail.com>
2023-07-29 09:37:36 +0200

    Init monorepo

---
+++ packages/main/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { add } from '@monorepo-app/shared'
+import { computed } from 'vue'
+
+const result = computed(() => add(1, 2))
+</script>
+
+<template>
+  <main>
+    Result:
+    {{ result }}
+  </main>
+</template>
